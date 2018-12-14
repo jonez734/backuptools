597cc405ab97fdd9781c152b23550b9bbb799ba5
J <jam@zoidtechnologies.com>
2018-12-14 01:23:34 -0500

    backups/syncbackups.py: initial revision

---
+++ syncbackups.py
@@ -0,0 +1,8 @@
+import os
+
+def main():
+    # sync from falcon to cyclops
+    # sync from cyclops to falcon
+    # sync from cyclops to thumbdrive if mounted
+    os.system("rsync --help")
+    pass
