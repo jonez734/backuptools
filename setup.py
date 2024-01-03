08ec25d57734c9337171c8f06504d6b4cd5a4300
Jeff MacDonald <jam@zoidtechnologies.com>
2024-01-03 18:42:02 -0500

    - backuptools/setup.py: updated to setuptools from distutils; updated 'author' and 'requires'

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
-
+from setuptools import setup
 import time
 
 v = time.strftime("%Y%m%d%H%M")
@@ -11,10 +10,10 @@ setup(
   description="a set of Python scripts to make backups",
   version=v,
   url="http://projects.zoidtechnologies.com/%s/" % (projectname),
-  author="zoid technologies",
+  author="zoidtechnologies.com",
   author_email="%s@projects.zoidtechnologies.com" % (projectname),
   scripts=["backupdatabase", "syncbackups"],
   py_modules=["backupdatabase", "syncbackups"],
-  requires=["ttyio4"],
+  requires=["bbsengine6"],
   license="GPLv2"
 )
